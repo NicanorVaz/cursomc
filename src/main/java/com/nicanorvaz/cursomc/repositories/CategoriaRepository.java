20b87bb72f2ac627c5b2ef07952c70bd598ddf09
Nicanor Vaz <nic.gerald@gmail.com>
2020-01-15 09:40:37 -0300

    Ciacao dos servico e repositorio

---
+++ src/main/java/com/nicanorvaz/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.nicanorvaz.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nicanorvaz.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
